c1bea309374e91d849998453601972d9a2ab933f
LXLY99 <ly2945706262@gmail.com>
2026-01-15 16:10:43 +0800

    初始化项目

---
+++ src/main/java/org/lxly/blog/LxlyBlogApplication.java
@@ -0,0 +1,13 @@
+package org.lxly.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxlyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxlyBlogApplication.class, args);
+    }
+
+}
